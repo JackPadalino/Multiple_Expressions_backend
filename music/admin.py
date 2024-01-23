c5a3aa1fe3aa6b237264a7b285bae0ee4522dbec
JackPadalino <pada0867@gmail.com>
2024-01-23 11:26:02 -0500

    added Tag, Track, and Video models

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import (Tag, Track, Video)
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Track)
+admin.site.register(Video)
