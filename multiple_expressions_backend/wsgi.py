4837d6c130d64473d89c7cadaa538976c9174f40
JackPadalino <pada0867@gmail.com>
2024-01-25 18:18:11 -0500

    simplified wsgi.py file to test deployment

--- multiple_expressions_backend/wsgi.py
+++ multiple_expressions_backend/wsgi.py
@@ -7,12 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# import os
+# from django.core.wsgi import get_wsgi_application
+# from static_ranges import Ranges # *
+# from dj_static import Cling, MediaCling # *
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multiple_expressions_backend.settings')
+# application = Ranges(Cling(MediaCling(get_wsgi_application()))) # *
+
 import os
 from django.core.wsgi import get_wsgi_application
-from static_ranges import Ranges # *
-from dj_static import Cling, MediaCling # *
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multiple_expressions_backend.settings')
-# application = Ranges(Cling(MediaCling(get_wsgi_application()))) # *
 application = get_wsgi_application()
 
 # * New setting to allow for partial requests (video looping and weeking)
