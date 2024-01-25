7f9c5512ba46598b4bfc056970e1ebc93f8b4bea
JackPadalino <pada0867@gmail.com>
2024-01-25 14:55:08 -0500

    updated settings to deploy to render

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-poetry install
+# Install dependencies
+pip install -r requirements.txt
 
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
