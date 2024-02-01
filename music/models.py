8616208810612909b02ada3e68a3d756befa2f9b
JackPadalino <pada0867@gmail.com>
2024-01-31 20:08:57 -0500

    added logic to delete track and video files and phoots from S3 bucket when deleting from Django backend

--- music/models.py
+++ music/models.py
@@ -1,6 +1,9 @@
+import os
 from django.db import models
 from django.core.validators import FileExtensionValidator
-from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
+import boto3 
 
 class Tag(models.Model):
     TAG_CHOICES = (
@@ -39,7 +42,7 @@ class Track(models.Model):
     def __str__(self):
         tags_str = ', '.join(tag.title for tag in self.tags.all())
         artists_str = ', '.join(artist.name for artist in self.artists.all())
-        return f"{self.title} (Tags: {tags_str}, Users: {artists_str})"
+        return f"{self.title} (Tags: {tags_str}, Artists: {artists_str})"
 
 class Video(models.Model):
     title = models.CharField(max_length=255, unique=True, blank=False)
@@ -60,4 +63,26 @@ class Video(models.Model):
     def __str__(self):
         tags_str = ', '.join(tag.title for tag in self.tags.all())
         artists_str = ', '.join(artist.name for artist in self.artists.all())
-        return f"{self.title} (Tags: {tags_str}, Users: {artists_str})"
+        return f"{self.title} (Tags: {tags_str}, Artists: {artists_str})"
+
+# delete audio files and images for deleted track from AWS S3 bucket
+@receiver(post_delete, sender=Track)
+def delete_track_from_s3_bucket(sender, instance, **kwargs):
+    aws_key_id = os.environ.get('ME_AWS_ACCESS_KEY_ID')
+    aws_secret_key = os.environ.get('ME_AWS_SECRET_ACCESS_KEY')
+    aws_bucket_name = os.environ.get('ME_AWS_STORAGE_BUCKET_NAME')
+
+    client = boto3.client('s3', aws_access_key_id=aws_key_id, aws_secret_access_key=aws_secret_key)
+    client.delete_object(Bucket=aws_bucket_name, Key=f'{instance.file}')
+    client.delete_object(Bucket=aws_bucket_name, Key=f'{instance.track_photo}')
+
+# delete video files and images for deleted track from AWS S3 bucket
+@receiver(post_delete, sender=Video)
+def delete_video_from_s3_bucket(sender, instance, **kwargs):
+    aws_key_id = os.environ.get('ME_AWS_ACCESS_KEY_ID')
+    aws_secret_key = os.environ.get('ME_AWS_SECRET_ACCESS_KEY')
+    aws_bucket_name = os.environ.get('ME_AWS_STORAGE_BUCKET_NAME')
+
+    client = boto3.client('s3', aws_access_key_id=aws_key_id, aws_secret_access_key=aws_secret_key)
+    client.delete_object(Bucket=aws_bucket_name, Key=f'{instance.file}')
+    client.delete_object(Bucket=aws_bucket_name, Key=f'{instance.video_photo}')
