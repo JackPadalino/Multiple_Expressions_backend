9669d17a489cadc959e5abeeb08eb9a70cefe39e
JackPadalino <pada0867@gmail.com>
2024-04-15 22:19:45 -0400

    updated routes and removed unecessary slash

--- music/urls.py
+++ music/urls.py
@@ -6,9 +6,9 @@ app_name="music"
 urlpatterns = [
     path('artists/<int:pk>', views.SingleArtistAPIView.as_view(), name='api-single-artist'),
     path('artists/all', views.AllArtistsAPIView.as_view(), name='api-all-artists'),
-    path('tracks/<int:pk>/', views.SingleTrackAPIView.as_view(), name='api-single-track'),
-    path('tracks/setup/', views.SetupTracksAPIView.as_view(), name='api-setup-tracks'),
+    path('tracks/<int:pk>', views.SingleTrackAPIView.as_view(), name='api-single-track'),
+    path('tracks/setup', views.SetupTracksAPIView.as_view(), name='api-setup-tracks'),
     path("tracks/all", views.AllTracksAPIView.as_view(), name="api-all-tracks"),
-    path('videos/<int:pk>/', views.SingleVideoAPIView.as_view(), name='api-single-video'),
+    path('videos/<int:pk>', views.SingleVideoAPIView.as_view(), name='api-single-video'),
     path("videos/all",views.AllVideosAPIView.as_view(),name="api-all-videos"),
 ]
